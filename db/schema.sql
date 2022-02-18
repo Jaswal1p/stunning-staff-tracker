09b0c722171d9ce5a84d8ab34dd1605f5b46f8bb
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-18 18:49:57 -0500

    minor corrections in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER,
-    CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 
@@ -31,9 +31,12 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+
   role_id INTEGER,
-  CONSTRAINT fk_party2 FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+
   manager_id INTEGER,
-  CONSTRAINT fk_party3 FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+  
   is_manager BOOLEAN NOT NULL
 );
